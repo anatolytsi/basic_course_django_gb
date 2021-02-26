e5803eae1e8cee1d2c4a679f9ab5b6e04ddad69d
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-02-26 21:02:19 +0100

    added media folder

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from mainapp.views import index, products
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path("", index, name="index"),  # Our web page is accessible via the main address and thus index fcn is called
     path("products/", products, name="products"),  # Our web page is accessible via /products
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
