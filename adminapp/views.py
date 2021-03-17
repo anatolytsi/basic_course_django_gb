01cd355282cc97de56090c37835bdc1cfbcaf8b1
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-03-17 14:02:48 +0100

    created controller for update users

--- adminapp/views.py
+++ adminapp/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.contrib import messages
 
 from authapp.models import User
-from adminapp.forms import UserAdminRegisterForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 
 def index(request):
@@ -32,8 +32,20 @@ def admin_users_create(request):
 
 
 # UPDATE
-def admin_users_update(request):
-    return render(request, "adminapp/admin-users-update-delete.html")
+def admin_users_update(request, user_id):
+    user = User.objects.get(id=user_id)
+    if request.method == "POST":
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Данные успешно сохраненны!")
+            return HttpResponseRedirect(reverse("admin_staff:admin_users"))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminProfileForm(instance=user)
+    context = {"form": form, "user": user}
+    return render(request, "adminapp/admin-users-update-delete.html", context)
 
 
 # DELETE
