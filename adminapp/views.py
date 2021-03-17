965a435b250154ba1220be9f4b0486ed45d5c75b
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-03-17 15:11:13 +0100

    added user_passes_test for all controllers in admin

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
 
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 
+@user_passes_test(lambda u: u.is_superuser, login_url="/")
 def index(request):
     return render(request, "adminapp/index.html")
 
 
 # READ
+@user_passes_test(lambda u: u.is_superuser, login_url="/")
 def admin_users(request):
     context = {"users": User.objects.all()}
     return render(request, "adminapp/admin-users-read.html", context)
 
 
 # CREATE
+@user_passes_test(lambda u: u.is_superuser, login_url="/")
 def admin_users_create(request):
     if request.method == "POST":
         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
@@ -32,6 +36,7 @@ def admin_users_create(request):
 
 
 # UPDATE
+@user_passes_test(lambda u: u.is_superuser, login_url="/")
 def admin_users_update(request, user_id):
     user = User.objects.get(id=user_id)
     if request.method == "POST":
@@ -49,6 +54,7 @@ def admin_users_update(request, user_id):
 
 
 # DELETE
+@user_passes_test(lambda u: u.is_superuser, login_url="/")
 def admin_users_delete(request, user_id):
     user = User.objects.get(id=user_id)
     if user.is_active:
