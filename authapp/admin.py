e20e33a578d2d58617fefc32f51078fa9c244a22
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-03-17 15:38:46 +0100

    added UserAdmin for Django

--- authapp/admin.py
+++ authapp/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from authapp.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "is_active")
+    fields = ("username", ("first_name", "last_name"), "email", "avatar", ("is_active", "is_superuser", "is_staff"), "last_login", "date_joined")
+    readonly_fields = ("last_login", "date_joined", )
+    ordering = ("username",)
+    search_fields = ("first_name", "last_name", "username")
