b28d80ffe2c257178451e7d7440724333ac0f85f
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-03-17 13:42:32 +0100

    created controller for create users

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 
 from authapp.models import User
+from adminapp.forms import UserAdminRegisterForm
 
 
 def index(request):
@@ -15,7 +18,17 @@ def admin_users(request):
 
 # CREATE
 def admin_users_create(request):
-    return render(request, "adminapp/admin-users-create.html")
+    if request.method == "POST":
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("admin_staff:admin_users"))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegisterForm()
+    context = {"form": form}
+    return render(request, "adminapp/admin-users-create.html", context)
 
 
 # UPDATE
