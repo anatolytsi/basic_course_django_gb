098dfc2ff5c39e0fdbf150bee249ec9de5397fd5
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-03-17 19:04:11 +0100

    added CBV for admin users

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from adminapp.views import (index, admin_users, admin_users_create, admin_users_update, admin_users_delete)
+from adminapp.views import (index, UserListView, UserCreateView, UserUpdateView, UserDeleteView)
 from adminapp.views import (admin_products, admin_products_create, admin_products_update, admin_products_delete)
 from adminapp.views import (admin_categories, admin_categories_create, admin_categories_update, admin_categories_delete)
 
@@ -8,10 +8,10 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path("", index, name="index"),
-    path("users/", admin_users, name="admin_users"),
-    path("users-create/", admin_users_create, name="admin_users_create"),
-    path("users-update/<int:user_id>/", admin_users_update, name="admin_users_update"),
-    path("users-delete/<int:user_id>/", admin_users_delete, name="admin_users_delete"),
+    path("users/", UserListView.as_view(), name="admin_users"),
+    path("users-create/", UserCreateView.as_view(), name="admin_users_create"),
+    path("users-update/<int:pk>/", UserUpdateView.as_view(), name="admin_users_update"),
+    path("users-delete/<int:pk>/", UserDeleteView.as_view(), name="admin_users_delete"),
     path("products/", admin_products, name="admin_products"),
     path("products-create/", admin_products_create, name="admin_products_create"),
     path("products-update/<int:product_id>/", admin_products_update, name="admin_products_update"),
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from authapp.models import User
 from mainapp.models import Product, ProductCategory
@@ -14,56 +17,62 @@ def index(request):
 
 
 # READ
-@user_passes_test(lambda u: u.is_superuser, login_url="/")
-def admin_users(request):
-    context = {"users": User.objects.all()}
-    return render(request, "adminapp/admin-users-read.html", context)
+class UserListView(ListView):
+    model = User
+    template_name = "adminapp/admin-users-read.html"
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser, login_url="/"))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
 # CREATE
-@user_passes_test(lambda u: u.is_superuser, login_url="/")
-def admin_users_create(request):
-    if request.method == "POST":
-        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse("admin_staff:admin_users"))
-        else:
-            print(form.errors)
-    else:
-        form = UserAdminRegisterForm()
-    context = {"form": form}
-    return render(request, "adminapp/admin-users-create.html", context)
+class UserCreateView(CreateView):
+    model = User
+    template_name = "adminapp/admin-users-create.html"
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy("admin_staff:admin_users")
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser, login_url="/"))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
 
 # UPDATE
-@user_passes_test(lambda u: u.is_superuser, login_url="/")
-def admin_users_update(request, user_id):
-    user = User.objects.get(id=user_id)
-    if request.method == "POST":
-        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Данные успешно сохраненны!")
-            return HttpResponseRedirect(reverse("admin_staff:admin_users"))
-        else:
-            print(form.errors)
-    else:
-        form = UserAdminProfileForm(instance=user)
-    context = {"form": form, "user": user}
-    return render(request, "adminapp/admin-users-update-delete.html", context)
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = "adminapp/admin-users-update-delete.html"
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy("admin_staff:admin_users")
+
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data()
+        context["title"] = "GeekShop - Редактирование пользователя"
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser, login_url="/"))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
 
 
 # DELETE
-@user_passes_test(lambda u: u.is_superuser, login_url="/")
-def admin_users_delete(request, user_id):
-    user = User.objects.get(id=user_id)
-    if user.is_active:
-        user.is_active = False
-    else:
-        user.is_active = True
-    user.save()
-    return HttpResponseRedirect(reverse("admin_staff:admin_users"))
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = "adminapp/admin-users-update-delete.html"
+    success_url = reverse_lazy("admin_staff:admin_users")
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.is_active:
+            self.object.is_active = False
+        else:
+            self.object.is_active = True
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser, login_url="/"))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserDeleteView, self).dispatch(request, *args, **kwargs)
 
 
 # READ
